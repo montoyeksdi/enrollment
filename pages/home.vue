646d8f36d31a5d27b453f97bb9ffa36b63ceb66e
montoyeksdi <raymund.sanchez.96@gmail.com>
2022-11-11 07:41:19 +0800

    update crud

--- pages/home.vue
+++ pages/home.vue
@@ -1,41 +1,48 @@
 <template>
 <body>
-<div id="app" style="background-color: green; height: 100vh">
-  <div class="container" style="background-color: white; height: 100vh; margin-left: 5%; z-index: 1; overflow: scroll">
-    <br>
-    <div class="form-inline" action="#" style="margin-top: 5%">
-    <input id="form-name" v-model="item.fname" type="text" placeholder="Firstname" class="form-control">
-    <input id="form-name" v-model="item.lname" type="text" placeholder="Lastname" class="form-control"> 
-    <input v-model="item.cnum" type="number" placeholder="Contact Number" class="form-control" v-on:keyup.enter="addItem">
-    <button @click="addItem" class="btn-add">ADD STUDENT</button>
+    <div class="fill-container" style="height: 100vh; background: orange">
+      <div class="add-container" style="height: 96vh; width: 28%; transform: translateY(13px); margin-left: 12px; border-radius: 20px; background: white; box-shadow: 5px 5px 5px rgb(0 0 0 / 30%);">
+        <p class="header-login" style="transform: translateY(15px); margin-left: 100px; margin-top: 60px">ADD STUDENTS</p>
+        <input id="form-name" v-model="item.fname" type="text" placeholder="Firstname" class="form-control" style="margin-left: 35px; transform: translateY(170px); width: 80%">
+        <input id="form-name" v-model="item.lname" type="text" placeholder="Lastname" class="form-control" style="margin-left: 35px; transform: translateY(180px); width: 80%"> 
+        <input v-model="item.cnum" type="number" placeholder="Contact Number" class="form-control" style="margin-left: 35px; transform: translateY(190px); width: 80%">
+        <input v-model="item.age" type="number" placeholder="Age" class="form-control" v-on:keyup.enter="addItem" style="margin-left: 35px; transform: translateY(200px); width: 80%">
+        <button style="margin-left: 130px; transform: translateY(250px); width: 30%" @click="addItem" class="btn-add">ADD</button>
+      </div>
+      <div class="view-table" style="height: 96vh; margin-left: 415px; margin-top: -94vh; width: 63%; border-radius: 20px; background: white; box-shadow: 5px 5px 5px rgb(0 0 0 / 30%); overflow: scroll">
+        <table class="table table-striped table-bordered table-sm" style="text-align: center; background-color: white; transform: translateY(20px); width: 95%; margin-left: 20px">
+          <thead style="background-color: #F5F5F5;; color: black">
+            <th> First Name</th>
+            <th> Last Name</th>
+            <th> Age</th>
+            <th> Contact</th>
+            <th style="background: orange; color: white; width: 200px"> Modify</th>
+          </thead>
+          <tr v-for="(item, index) in items" :key="item">
+            <td>
+              <input v-if="item.edit" v-model="item.fname"  type="text" v-on:keyup.enter="item.edit = !item.edit">
+              <span v-else>{{item.fname}} </span>
+            </td>
+            <td>
+              <input v-if="item.edit" v-model="item.lname" type="text" v-on:keyup.enter="item.edit = !item.edit">
+              <span v-else>{{item.lname}} </span>
+            </td>
+            <td>
+              <input v-if="item.edit" v-model="item.age" type="text" v-on:keyup.enter="item.edit = !item.edit">
+              <span v-else>{{item.age}} </span>
+            </td>
+            <td>
+              <input v-if="item.edit" v-model="item.cnum" type="text" v-on:keyup.enter="item.edit = !item.edit">
+              <span v-else>{{item.cnum}} </span>
+            </td>
+            <td>
+              <button @click="item.edit = !item.edit" class="btn btn-info" style="width: 60px"><i class="far fa-edit">EDIT</i></button>
+              <button @click="removeItem(index)" class="btn btn-danger" style="width: 90px"><i class="far fa-trash-alt">REMOVE</i></button>
+            </td>
+          </tr>
+        </table>
+      </div>
     </div>
-    <br><br>
-    <table class="table table-striped table-bordered table-sm" style="text-align: center; background-color: orange">
-      <thead style="background-color: #F5F5F5;; color: black">
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Contact No.</th>
-        <th class="col-2">Modify User</th>
-      </thead>
-      <tr v-for="(item, index) in items" :key="item">
-        <td>
-          <input v-if="item.edit" v-model="item.fname"  type="text" v-on:keyup.enter="item.edit = !item.edit">
-          <span v-else>{{item.fname}} </span>
-        </td>
-        <td>
-          <input v-if="item.edit" v-model="item.lname" type="text" v-on:keyup.enter="item.edit = !item.edit">
-          <span v-else>{{item.lname}} </span>
-        </td>
-        <td>
-          <input v-if="item.edit" v-model="item.cnum" type="text" v-on:keyup.enter="item.edit = !item.edit">
-          <span v-else>{{item.cnum}} </span>
-        </td>
-        <td><button @click="item.edit = !item.edit" class="btn btn-info"><i class="far fa-edit">EDIT</i></button>
-          <button @click="removeItem(index)" class="btn btn-danger"><i class="far fa-trash-alt">REMOVE</i></button></td>
-      </tr>
-    </table>
-  </div>
-</div>
 </body>
 </template>
 
@@ -44,14 +51,14 @@ export default {
     name: 'Home',
     data() {
     return {
-      item: {fname: "", lname: "", cnum: "", edit: false},
+      item: {fname: "", lname: "", cnum: "", age: "", edit: false},
       items: []
     }
   },
   methods:{
     addItem() {
       this.items.push({
-        fname:this.item.fname, lname:this.item.lname, cnum:this.item.cnum, edit: false}
+        fname:this.item.fname, lname:this.item.lname, cnum:this.item.cnum, age:this.item.age, edit: false}
         );
       this.item = [];
     },
